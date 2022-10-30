ee88ba84f9688ebb7d413975de7a4240c34dfd5c
Vladimir Liamin <LyaminVA@yandex.ru>
2022-10-30 20:22:46 +0300

    Add files to wiki

---
+++ src/Models/Broker/Common/ICheckArticlesExistence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Common
+{
+    public interface ICheckArticlesExistence
+    {
+        List<Guid> ArticlesIds { get; }
+
+        static object CreateObj(List<Guid> articlesIds)
+        {
+            return new
+            {
+                ArticlesIds = articlesIds
+            };
+        }
+    }
+}
