4e437d9cc38c3def23aa0c3be9dca8f5602d470c
Vladimir Liamin <LyaminVA@yandex.ru>
2023-01-27 00:24:34 +0300

    added MaxSizeCompress to ResizeParameters

--- src/Models/Broker/Models/Image/ResizeParameters.cs
+++ src/Models/Broker/Models/Image/ResizeParameters.cs
@@ -3,13 +3,16 @@
     public class ResizeParameters
     {
         public int? MaxResizeValue { get; }
+        public int? MaxSizeCompress { get; }
         public PreviewParameters PreviewParameters { get; }
 
         public ResizeParameters(
             int? maxResizeValue,
+            int? maxSizeCompress,
             PreviewParameters previewParameters)
         {
             MaxResizeValue = maxResizeValue;
+            MaxSizeCompress = maxSizeCompress;
             PreviewParameters = previewParameters;
         }
     }
