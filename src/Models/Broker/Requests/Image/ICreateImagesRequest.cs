8f08d2c1803640282c52d2256caad8affd5b5724
Vladimir Liamin <LyaminVA@yandex.ru>
2023-01-26 17:38:01 +0300

    small fixes

--- src/Models/Broker/Requests/Image/ICreateImagesRequest.cs
+++ src/Models/Broker/Requests/Image/ICreateImagesRequest.cs
@@ -1,5 +1,4 @@
-﻿using DigitalOffice.Models.Broker.Models.Image;
-using LT.DigitalOffice.Models.Broker.Enums;
+﻿using LT.DigitalOffice.Models.Broker.Enums;
 using LT.DigitalOffice.Models.Broker.Models.Image;
 using System;
 using System.Collections.Generic;
@@ -10,20 +9,17 @@ namespace LT.DigitalOffice.Models.Broker.Requests.Image
     {
         List<CreateImageData> Images { get; }
         ImageSource ImageSource { get; }
-        public ResizeParameters ResizeParameters { get; }
         public Guid CreatedBy { get; }
 
         static object CreateObj(
             List<CreateImageData> images,
             ImageSource imageSource,
-            ResizeParameters resizeParameters,
             Guid createdBy)
         {
             return new
             {
                 Images = images,
                 ImageSource = imageSource,
-                ResizeParameters = resizeParameters,
                 CreatedBy = createdBy
             };
         }
