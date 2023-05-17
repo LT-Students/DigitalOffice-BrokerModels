311a636661abe42332a3c81d2b0f95217b0fd84a
Vladimir Liamin <LyaminVA@yandex.ru>
2023-05-17 22:09:54 +0300

    Add ICheckEventsExistence

---
+++ src/Models/Broker/Common/ICheckEventsExistence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Common
+{
+    public interface ICheckEventsExistence
+    {
+        List<Guid> EventsIds { get; }
+
+        static object CreateObj(List<Guid> eventsIds)
+        {
+            return new
+            {
+                EventsIds = eventsIds
+            };
+        }
+    }
+}
