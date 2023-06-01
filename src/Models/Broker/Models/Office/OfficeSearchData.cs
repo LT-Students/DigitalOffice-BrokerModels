6b0769a7023028a91acede60115ad5154bcaecfa
Liza Sinitsyna <108836868+liza459@users.noreply.github.com>
2023-06-01 15:06:13 +0300

    fix

--- src/Models/Broker/Models/Office/OfficeSearchData.cs
+++ src/Models/Broker/Models/Office/OfficeSearchData.cs
@@ -6,10 +6,12 @@ namespace DigitalOffice.Models.Broker.Models.Office
     {
         public Guid Id { get; }
         public string Name { get; }
-        public OfficeSearchData(Guid id, string name)
+        public string Address { get; }
+        public OfficeSearchData(Guid id, string name, string address)
         {
             Id = id;
             Name = name;
+            Address = address;
         }
     }
 }
--- src/Models/Broker/Responses/Search/ISearchResponse.cs
+++ src/Models/Broker/Responses/Search/ISearchResponse.cs
@@ -5,12 +5,14 @@ namespace LT.DigitalOffice.Models.Broker.Responses.Search
     public interface ISearchResponse<T>
     {
         List<T> Values { get; }
+        int TotalCount { get; }
 
         static object CreateObj(List<T> values)
         {
             return new
             {
                 Values = values,
+                TotalCount = values.Count
             };
         }
     }
