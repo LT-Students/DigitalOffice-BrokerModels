9d1e84fe98fccd9d38777eb0649ac1ccb34276be
ndoriomedov <doriomedov213@gmail.com>
2023-06-07 00:11:41 +0300

    add departmentExtendedData for company structure

---
+++ src/Models/Broker/Models/Department/DepartmnetUserRolesData.cs
@@ -0,0 +1,22 @@
+﻿using DigitalOffice.Models.Broker.Enums;
+using LT.DigitalOffice.Models.Broker.Enums;
+using System;
+
+namespace DigitalOffice.Models.Broker.Models.Department;
+
+public class DepartmnetUserRolesData
+{
+  public Guid Id { get; set; }
+  public DepartmentUserRole Role { get; set; }
+  public DepartmentUserAssignment Assignment { get; set; }
+
+  public DepartmnetUserRolesData(
+    Guid id,
+    DepartmentUserRole role,
+    DepartmentUserAssignment assignment)
+  {
+    Id = id;
+    Role = role;
+    Assignment = assignment;
+  }
+}
