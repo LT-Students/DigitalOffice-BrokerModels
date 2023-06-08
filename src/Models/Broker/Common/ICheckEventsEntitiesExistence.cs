e5554616f75f25c227efa817e37a89eead6e5d6a
Artem Lopatin <artx7@outlook.com>
2023-06-08 17:44:48 +0300

    resolved conflicts and fixed interfaces

---
+++ src/Models/Broker/Common/ICheckEventsEntitiesExistence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Common
+{
+    public interface ICheckEventsEntitiesExistence
+    {
+        List<Guid> EntitiesIds { get; }
+
+        static object CreateObj(List<Guid> entitiesIds)
+        {
+            return new
+            {
+                EntitiesIds = entitiesIds
+            };
+        }
+    }
+}
