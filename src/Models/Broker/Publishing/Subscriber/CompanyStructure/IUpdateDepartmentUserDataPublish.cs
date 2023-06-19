f2ea3f3dfd614e2d89fa7a06fd08142a2a8e6e1d
ndoriomedov <doriomedov213@gmail.com>
2023-06-19 15:33:17 +0300

    add IUpdateDepartmentDataPublish, IUpdateUserDataPublish, IUpdateDepartmentUserDataPublish

---
+++ src/Models/Broker/Publishing/Subscriber/CompanyStructure/IUpdateDepartmentUserDataPublish.cs
@@ -0,0 +1,34 @@
+﻿using DigitalOffice.Models.Broker.Enums;
+using LT.DigitalOffice.Models.Broker.Enums;
+using System;
+
+namespace DigitalOffice.Models.Broker.Publishing.Subscriber.CompanyStructure;
+
+public interface IUpdateDepartmentUserDataPublish
+{
+  Guid UserId { get; set; }
+  Guid? DepartmentId { get; set; }
+  DepartmentUserRole? Role { get; set; }
+  DepartmentUserAssignment? Assignment { get; set; }
+  bool? IsActive { get; set; }
+  Guid ModifiedBy { get; set; }
+
+  static object CreateObj(
+    Guid userId,
+    Guid? departmentId,
+    DepartmentUserRole? role,
+    DepartmentUserAssignment? assignment,
+    bool? isActive,
+    Guid modifiedBy)
+  {
+    return new
+    {
+      UserId = userId,
+      DepartmentId = departmentId,
+      Role = role,
+      Assignment = assignment,
+      IsActive = isActive,
+      ModifiedBy = modifiedBy
+    };
+  }
+}
---
+++ src/Models/Broker/Publishing/Subscriber/CompanyStructure/IUpdateUserDataPublish.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DigitalOffice.Models.Broker.Publishing.Subscriber.CompanyStructure;
+
+public interface IUpdateUserDataPublish
+{
+  Guid UserId { get; set; }
+  string FirstName { get; set; }
+  string MiddleName { get; set; }
+  string LastName { get; set; }
+  Guid? AvatarId { get; set; }
+  Guid ModifiedBy { get; set; }
+
+  static object CreateObj(
+    Guid userId,
+    string firstName,
+    string middleName,
+    string lastName,
+    Guid? avatarId,
+    Guid modifiedBy)
+  {
+    return new
+    {
+      UserId = userId,
+      FirstName = firstName,
+      MiddleName = middleName,
+      LastName = lastName,
+      AvatarId = avatarId,
+      ModifiedBy = modifiedBy
+    };
+  }
+}
