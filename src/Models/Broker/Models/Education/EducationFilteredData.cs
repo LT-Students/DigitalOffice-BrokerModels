c6b4f4227975f3cae2ea92987e927a60ab17cea1
Artem Lopatin <artx7@outlook.com>
2023-07-11 12:19:26 +0300

    fix

--- src/Models/Broker/Models/Education/EducationFilteredData.cs
+++ src/Models/Broker/Models/Education/EducationFilteredData.cs
@@ -4,11 +4,13 @@ namespace DigitalOffice.Models.Broker.Models.Education;
 
 public class EducationFilteredData
 {
+  public Guid Id { get; }
   public string UniversityName { get; }
   public Guid UserId { get; }
 
-  public EducationFilteredData(string universityName, Guid userId)
+  public EducationFilteredData(Guid id, string universityName, Guid userId)
   {
+    Id = id;
     UniversityName = universityName;
     UserId = userId;
   }
