d7d676bc5aae2e813b33a59eb1f54b6ef32799aa
ndoriomedov <doriomedov213@gmail.com>
2023-07-19 16:36:13 +0300

    add IUpdateUserPositionDataPublish

---
+++ src/Models/Broker/Publishing/Subscriber/CompanyStructure/IUpdateUserPositionDataPublish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DigitalOffice.Models.Broker.Publishing.Subscriber.CompanyStructure;
+public interface IUpdateUserPositionDataPublish
+{
+  Guid UserId { get; set; }
+  Guid? PositionId { get; set; }
+  Guid ModifiedBy { get; set; }
+
+  static object CreateObj(
+    Guid userId,
+    Guid? positionId,
+    Guid modifiedBy)
+  {
+    return new
+    {
+      UserId = userId,
+      PositionId = positionId,
+      ModifiedBy = modifiedBy
+    };
+  }
+}
