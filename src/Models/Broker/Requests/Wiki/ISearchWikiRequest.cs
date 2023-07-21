94c9948e8b066b58a74b8efd52c2a5c072f56c0c
Liza Sinitsyna <108836868+liza459@users.noreply.github.com>
2023-07-21 14:33:21 +0300

    fix

---
+++ src/Models/Broker/Requests/Wiki/ISearchWikiRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalOffice.Models.Broker.Requests.Wiki;
+public interface ISearchWikiRequest
+{
+  string[] Value { get; }
+
+  static object CreateObj(string[] value)
+  {
+    return new
+    {
+      Value = value
+    };
+  }
+}
