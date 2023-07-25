c581ee3a58b3b9912bc45972939a85dac024dfe4
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2023-07-26 00:13:01 +0300

    add models for updating user contacts in CompanyStructureService

---
+++ src/Models/Broker/Publishing/Subscriber/CompanyStructure/IRemoveUserCommunicationDataPublish.cs
@@ -0,0 +1,21 @@
+﻿using DigitalOffice.Models.Broker.Models.User;
+using System;
+
+namespace DigitalOffice.Models.Broker.Publishing.Subscriber.CompanyStructure;
+
+public interface IRemoveUserCommunicationDataPublish
+{
+  Guid UserId { get; set; }
+  UserCommunicationData CommunicationData { get; set; }
+
+  static object CreateObj(
+    Guid userId,
+    UserCommunicationData communicationData)
+  {
+    return new
+    {
+      UserId = userId,
+      CommunicationData = communicationData
+    };
+  }
+}
---
+++ src/Models/Broker/Publishing/Subscriber/CompanyStructure/IUpdateUserCommunicationDataPublish.cs
@@ -0,0 +1,21 @@
+﻿using DigitalOffice.Models.Broker.Models.User;
+using System;
+
+namespace DigitalOffice.Models.Broker.Publishing.Subscriber.CompanyStructure;
+
+public interface IUpdateUserCommunicationDataPublish
+{
+  Guid UserId { get; set; }
+  UserCommunicationData CommunicationData { get; set; }
+
+  static object CreateObj(
+    Guid userId,
+    UserCommunicationData communicationData)
+  {
+    return new
+    {
+      UserId = userId,
+      CommunicationData = communicationData
+    };
+  }
+}
