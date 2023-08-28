c5222941c76ce65c3289a5f23c7ff9610f58fb68
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2023-08-28 14:03:45 +0300

    add check existence request

---
+++ src/Models/Broker/Common/ICheckClaimsExistence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Common;
+
+public interface ICheckClaimsExistence
+{
+  List<Guid> ClaimIds { get; }
+
+  static object CreateObj(List<Guid> claimIds)
+  {
+    return new
+    {
+      ClaimIds = claimIds
+    };
+  }
+}
