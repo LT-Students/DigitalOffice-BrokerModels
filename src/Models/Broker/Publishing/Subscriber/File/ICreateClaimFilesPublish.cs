9a995b641d41aa6c2883cb390feb97aacdc5f286
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2023-08-28 13:54:55 +0300

    add create file publish

---
+++ src/Models/Broker/Publishing/Subscriber/File/ICreateClaimFilesPublish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Publishing.Subscriber.File;
+
+public interface ICreateClaimFilesPublish
+{
+  List<Guid> FilesIds { get; }
+  Guid ClaimId { get; }
+
+  static object CreateObj(List<Guid> filesIds, Guid claimId)
+  {
+    return new
+    {
+      FilesIds = filesIds,
+      ClaimId = claimId
+    };
+  }
+}
