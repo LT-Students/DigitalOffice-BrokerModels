9b22011b73bc03559b8d8f9e83cc22738ebab495
Vladimir Liamin <LyaminVA@yandex.ru>
2023-10-18 02:28:39 +0300

    Add request on survey users

---
+++ src/Models/Broker/Requests/Survey/IGetSurveyUsersRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalOffice.Models.Broker.Requests.Survey;
+
+public interface IGetSurveyUsersRequest
+{
+  Guid SurveyId { get; }
+
+  static object CreateObj(Guid surveyId)
+  {
+    return new
+    {
+      SurveyId = surveyId
+    };
+  }
+}
---
+++ src/Models/Broker/Responses/Survey/IGetSurveyUsersResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Responses.Survey;
+
+public interface IGetSurveyUsersResponse
+{
+  List<Guid> UsersIds { get; }
+
+  static object CreateObj(List<Guid> usersIds)
+  {
+    return new
+    {
+      UsersIds = usersIds
+    };
+  }
+}
