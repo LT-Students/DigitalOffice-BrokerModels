bdc876d50c706e53341b8b02e955823e907d7222
Solovyov Dmitry <dsolovyov@foss.dk>
2024-01-19 11:40:00 +0300

    add ICreateFiles request/response

---
+++ src/Models/Broker/Requests/File/ICreateFilesRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Models.Broker.Requests.File
+{
+  public interface ICreateFilesRequest
+  {
+    List<Guid> FilesIds { get; }
+    Guid EntityId { get; }
+
+    static object CreateObj(List<Guid> filesIds, Guid entityId)
+    {
+      return new
+      {
+        FilesIds = filesIds,
+        EntityId = entityId
+      };
+    }
+  }
+}
---
+++ src/Models/Broker/Responses/File/ICreateFilesResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Models.Broker.Responses.File
+{
+  public interface ICreateFilesResponse
+  {
+    List<Guid> FilesIds { get; set; }
+    
+    static object CreateObj(List<Guid> filesIds)
+    {
+      return new
+      {
+        FilesIds = filesIds,
+      };
+    }
+  }
+}
