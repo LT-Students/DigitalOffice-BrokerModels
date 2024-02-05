8d20c22350782e524b3072530446ac3c235971e2
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2024-02-05 13:12:32 +0300

    add IFilterUsersRequest and response

---
+++ src/Models/Broker/Requests/Filter/IFilterUsersRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DigitalOffice.Models.Broker.Requests.Filter
+{
+  public interface IFilterUsersRequest
+  {
+    string FullNameIncludeSubstring { get; }
+    bool? IsActive { get; }
+    bool? IsPending { get; }
+    Guid? GenderId { get; }
+    bool IncludeCommunications { get; }
+
+    static object CreateObj(
+      string fullNameIncludeSubstring = null,
+      bool? isActive = true,
+      Guid? genderId = null,
+      bool? isPending = null,
+      bool includeCommunications = false)
+    {
+      return new {
+        FullNameIncludeSubstring = fullNameIncludeSubstring,
+        IsActive = isActive,
+        IsPending = isPending,
+        GenderId = genderId,
+        IncludeCommunications = includeCommunications
+      };
+    }
+  }
+}
---
+++ src/Models/Broker/Responses/Filter/IFilterUsersResponse.cs
@@ -0,0 +1,18 @@
+﻿using DigitalOffice.Models.Broker.Models.User;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Responses.Filter
+{
+  public class IFilterUsersResponse
+  {
+    List<UserData> UsersData { get; }
+
+    static object CreateObj(List<UserData> usersData)
+    {
+      return new
+      {
+        UsersData = usersData,
+      };
+    }
+  }
+}
