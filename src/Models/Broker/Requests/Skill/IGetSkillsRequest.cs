38bf6f343ac3daa6533769b9043f96abaa4ea20c
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2024-02-07 17:25:44 +0300

    add IGetSkills request and response

---
+++ src/Models/Broker/Requests/Skill/IGetSkillsRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Models.Broker.Requests.Skill
+{
+  public interface IGetSkillsRequest
+  {
+    List<Guid> UserIds { get; }
+
+    static object CreateObj(List<Guid> userIds)
+    {
+      return new
+      {
+        UserIds = userIds,
+      };
+    }
+  }
+}
---
+++ src/Models/Broker/Responses/Skill/IGetSkillsResponse.cs
@@ -0,0 +1,18 @@
+﻿using LT.DigitalOffice.Models.Broker.Models.Skill;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Models.Broker.Responses.Skill
+{
+  public interface IGetSkillsResponse
+  {
+    List<SkillFilteredData> Skills { get; }
+
+    static object CreateObj(List<SkillFilteredData> skills)
+    {
+      return new
+      {
+        Skills = skills
+      };
+    }
+  }
+}
