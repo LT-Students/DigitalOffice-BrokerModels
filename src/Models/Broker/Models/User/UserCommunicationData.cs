469c9f897d950630bb5672ef1e8a4d9441a14213
Vladimir Liamin <LyaminVA@yandex.ru>
2024-02-17 13:27:23 +0300

    Fix IFilterUsersResponse

--- src/Models/Broker/Models/User/UserCommunicationData.cs
+++ src/Models/Broker/Models/User/UserCommunicationData.cs
@@ -4,11 +4,11 @@ namespace DigitalOffice.Models.Broker.Models.User;
 
 public class UserCommunicationData
 {
-  public Guid CommunicationId { get; }
+  public Guid? CommunicationId { get; }
   public int Type { get; }
   public string Value { get; }
   
-  public UserCommunicationData(Guid communicationId, int type, string value)
+  public UserCommunicationData(int type, string value, Guid? communicationId = null)
   {
     CommunicationId = communicationId;
     Type = type;
