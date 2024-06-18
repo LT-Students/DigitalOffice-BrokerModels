6141de521bb3b382c723962ae4ec73141ef677c3
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-06-18 13:10:23 +0300

    Add Name to DigestFileData

--- src/Models/Broker/Models/Digest/DigestFileData.cs
+++ src/Models/Broker/Models/Digest/DigestFileData.cs
@@ -6,11 +6,13 @@ namespace DigitalOffice.Models.Broker.Models.Digest;
 public class DigestFileData
 {
   public Guid Id { get; }
+  public string Name { get; }
   public Locale Localization { get; }
 
-  public DigestFileData(Guid id, Locale localization)
+  public DigestFileData(Guid id, string name, Locale localization)
   {
     Id = id;
+    Name = name;
     Localization = localization;
   }
 }
