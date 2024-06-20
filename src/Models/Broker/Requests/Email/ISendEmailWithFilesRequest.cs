08ccbbcf351c6f832cc630c8053d56136ee92b00
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-06-20 15:08:24 +0300

    add ISendEmailWithFilesRequest

---
+++ src/Models/Broker/Requests/Email/ISendEmailWithFilesRequest.cs
@@ -0,0 +1,31 @@
+﻿using DigitalOffice.Models.Broker.Models.Digest;
+using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Requests.Email;
+
+public interface ISendEmailWithFilesRequest
+{
+  string Receiver { get; }
+  string Subject { get; }
+  string Text { get; }
+  List<DigestFileData> Files { get; }
+  Guid? SenderId { get; }
+
+  static object CreateObj(
+      string receiver,
+      string subject,
+      string text,
+      List<DigestFileData> files,
+      Guid? senderId = null)
+  {
+    return new
+    {
+      SenderId = senderId,
+      Receiver = receiver,
+      Subject = subject,
+      Text = text,
+      Files = files
+    };
+  }
+}
