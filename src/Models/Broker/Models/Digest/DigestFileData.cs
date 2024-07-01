d48396896cdbb5b53eaf7a4c3d03147a9d3077c5
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-07-01 15:36:16 +0300

    Add Path to DigestFileData

--- src/Models/Broker/Models/Digest/DigestFileData.cs
+++ src/Models/Broker/Models/Digest/DigestFileData.cs
@@ -8,13 +8,15 @@ public class DigestFileData
   public Guid Id { get; }
   public string Name { get; }
   public string Extension { get; }
+  public string Path { get; }
   public Locale Localization { get; }
 
-  public DigestFileData(Guid id, string name, string extension, Locale localization)
+  public DigestFileData(Guid id, string name, string extension, string path, Locale localization)
   {
     Id = id;
     Name = name;
     Extension = extension;
+    Path = path;
     Localization = localization;
   }
 }
