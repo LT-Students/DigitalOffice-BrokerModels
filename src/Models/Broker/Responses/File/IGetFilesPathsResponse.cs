f64dcc9d732f1c47fbe75f04585478f4bbc982c0
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-08-30 15:36:03 +0300

    add IGetFilesPathsResponse

---
+++ src/Models/Broker/Responses/File/IGetFilesPathsResponse.cs
@@ -0,0 +1,17 @@
+﻿using DigitalOffice.Models.Broker.Models.File;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Responses.File;
+
+public interface IGetFilesPathsResponse
+{
+  List<FilePathData> FilesPathsData { get; }
+
+  static object CreateObj(List<FilePathData> filesPathsData)
+  {
+    return new
+    {
+      FilesPathsData = filesPathsData
+    };
+  }
+}
