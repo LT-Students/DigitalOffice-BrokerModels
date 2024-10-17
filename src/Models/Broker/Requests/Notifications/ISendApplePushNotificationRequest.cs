917697acac7585e77842ad8471ee1995c136b313
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2024-10-17 16:13:25 +0300

    update ISendApplePushNotificationRequest

---
+++ src/Models/Broker/Requests/Notifications/ISendApplePushNotificationRequest.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Models.Broker.Requests.Notifications
+{
+  public interface ISendApplePushNotificationRequest
+  {
+    /// <summary>
+    /// Users.
+    /// </summary>
+    List<Guid> UserIds { get; }
+
+    /// <summary>
+    /// Title.
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Subtitle.
+    /// </summary>
+    public string Subtitle { get; set; }
+
+    /// <summary>
+    /// Body.
+    /// </summary>
+    public string Body { get; set; }
+
+    /// <summary>
+    /// Type.
+    /// </summary>
+    public string Type { get; set; }
+
+    /// <summary>
+    /// Badge value.
+    /// </summary>
+    public int? Badge { get; set; }
+
+    /// <summary>
+    /// Category.
+    /// </summary>
+    public string Category { get; set; }
+
+    /// <summary>
+    /// Expiration.
+    /// </summary>
+    public DateTimeOffset? Expiration { get; set; }
+
+    /// <summary>
+    /// Priority.
+    /// </summary>
+    public int? Priority { get; set; }
+
+    /// <summary>
+    /// Collapse.
+    /// </summary>
+    public string CollapseId { get; set; }
+
+    /// <summary>
+    /// Custom properties.
+    /// </summary>
+    public Dictionary<string, CustomProperty> CustomProperties { get; set; }
+
+    /// <summary>
+    /// Whether to send to a development center.
+    /// </summary>
+    public bool IsDev { get; set; }
+
+    static object CreateObj(
+      List<Guid> userIds,
+      string title,
+      string subtitle,
+      string body,
+      string type = "Alert",
+      int? badge = null,
+      string category = null,
+      DateTimeOffset? expiration = null,
+      int? priority = null,
+      string collapseId = null,
+      Dictionary<string, CustomProperty> customProperties = null,
+      bool isDev = true)
+    {
+      return new
+      {
+        UserIds = userIds,
+        Title = title,
+        Subtitle = subtitle,
+        Body = body,
+        Type = type,
+        Badge = badge,
+        Category = category,
+        Expiration = expiration,
+        Priority = priority,
+        CollapseId = collapseId,
+        CustomProperties = customProperties,
+        IsDev = isDev
+      };
+    }
+  }
+}
