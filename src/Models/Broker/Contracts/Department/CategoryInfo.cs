289fbf69681b64e5156c60df1e2e3fdc95218d6b
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2024-12-04 16:42:01 +0300

    Add Contracts\Department

---
+++ src/Models/Broker/Contracts/Department/CategoryInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalOffice.Models.Broker.Contracts.Department
+{
+  public class CategoryInfo
+  {
+    public Guid Id { get; }
+    public string Name { get; }
+
+    public CategoryInfo(
+      Guid id,
+      string name)
+    {
+      Id = id;
+      Name = name;
+    }
+  }
+}
---
+++ src/Models/Broker/Contracts/Department/DepartmentResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalOffice.Models.Broker.Contracts.Department
+{
+  public class DepartmentResponse
+  {
+    public Guid Id { get; }
+    public string Name { get; }
+    public string ShortName { get; }
+    public string Description { get; }
+    public bool IsActive { get; }
+    public Guid? ParentId { get; }
+    public CategoryInfo Category { get; }
+    public List<DepartmentUserInfo> Users { get; }
+
+    public DepartmentResponse(
+      Guid id,
+      string name,
+      string shortName,
+      string description,
+      bool isActive,
+      Guid? parentId,
+      CategoryInfo category,
+      List<DepartmentUserInfo> users = null)
+    {
+      Id = id;
+      Name = name;
+      ShortName = shortName;
+      Description = description;
+      IsActive = isActive;
+      ParentId = parentId;
+      Category = category;
+      Users = users;
+    }
+  }
+}
