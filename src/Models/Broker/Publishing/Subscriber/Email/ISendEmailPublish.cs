49b3f10e89ee2f7c645f22f599c6856b08d74753
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2025-02-06 14:28:08 +0300

    add

---
+++ src/Models/Broker/Publishing/Subscriber/Email/ISendEmailPublish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Email;
+
+
+public interface ISendEmailPublish
+{
+  string Receiver { get; }
+  string Subject { get; }
+  string Text { get; }
+  Guid? SenderId { get; }
+
+  static object CreateObj(
+      string receiver,
+      string subject,
+      string text,
+      Guid? senderId = null)
+  {
+    return new
+    {
+      SenderId = senderId,
+      Receiver = receiver,
+      Subject = subject,
+      Text = text
+    };
+  }
+}
