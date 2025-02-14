702c2c348e5c4483fd722598ab515d6d53e3eb0b
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2025-02-14 11:30:08 +0300

    add

---
+++ src/Models/Contracts/Time/WorkTimeMonthLimitInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DigitalOffice.Models.Contracts.Time;
+
+public record WorkTimeMonthLimitInfo
+{
+  /// <summary>
+  /// Work time month limit id
+  /// </summary>
+  public Guid Id { get; set; }
+
+  /// <summary>
+  /// Work time month limit month
+  /// </summary>
+  public int Month { get; set; }
+
+  /// <summary>
+  /// Work time month limit year
+  /// </summary>
+  public int Year { get; set; }
+
+  /// <summary>
+  /// Work time month limit norm hours
+  /// </summary>
+  public float NormHours { get; set; }
+
+  /// <summary>
+  /// Work time month limit holidays
+  /// </summary>
+  public string Holidays { get; set; }
+}
