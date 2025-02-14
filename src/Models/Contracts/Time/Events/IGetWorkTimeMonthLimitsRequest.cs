5c101f2b10f37a554f90a1d527eb070dd9e1e2d6
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2025-02-14 11:38:55 +0300

    add events

---
+++ src/Models/Contracts/Time/Events/IGetWorkTimeMonthLimitsRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace DigitalOffice.Models.Contracts.Time.Events;
+
+public interface IGetWorkTimeMonthLimitsRequest
+{
+  /// <summary>
+  /// Year of limits.
+  /// </summary>
+  public int? Year { get; set; }
+
+  /// <summary>
+  /// Month of limits.
+  /// </summary>
+  public int? Month { get; set; }
+
+  static object CreateObj(
+    int? year,
+    int? month)
+  {
+    return new
+    {
+      Year = year,
+      Month = month
+    };
+  }
+}
