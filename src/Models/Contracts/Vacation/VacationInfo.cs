d306aed934eebd1f42752bd85f2be0d62334c057
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2025-02-19 13:09:25 +0300

    add

--- src/Models/Contracts/Vacation/VacationInfo.cs
+++ src/Models/Contracts/Vacation/VacationInfo.cs
@@ -7,12 +7,14 @@ namespace DigitalOffice.Models.Contracts.Vacation
     public Guid UserId { get; init; }
     public DateOnly StartDateUtc { get; init; }
     public DateOnly EndDateUtc { get; init; }
+    public VacationType Type { get; set; }
     public string Comment { get; init; }
-    
+
     public VacationInfo(
       Guid userId,
       DateOnly startDateUtc,
       DateOnly endDateUtc,
+      VacationType type,
       string comment)
     {
       UserId = userId;
---
+++ src/Models/Contracts/Vacation/VacationType.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DigitalOffice.Models.Contracts.Vacation
+{
+  [JsonConverter(typeof(StringEnumConverter))]
+  public enum VacationType
+  {
+    Planned,
+    Actual,
+    SickLeave,
+    Training,
+    Idle,
+    Prolonged,
+    Decree
+  }
+}
