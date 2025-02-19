75b7e10750c9ee241806509320d2009c088c8119
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.ru>
2025-02-19 16:18:27 +0300

    add

--- src/Models/Contracts/Vacation/VacationType.cs
+++ src/Models/Contracts/Vacation/VacationType.cs
@@ -6,12 +6,12 @@ namespace DigitalOffice.Models.Contracts.Vacation
   [JsonConverter(typeof(StringEnumConverter))]
   public enum VacationType
   {
-    Planned,
-    Actual,
-    SickLeave,
-    Training,
-    Idle,
-    Prolonged,
-    Decree
+    Planned = 0,
+    Actual = 1,
+    SickLeave = 2,
+    Training = 3,
+    Idle = 4,
+    Prolonged = 5,
+    Decree = 6
   }
 }
