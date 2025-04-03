a6b11110f66f93e2db6e59ca58b0619b62eac8fc
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2025-04-03 18:32:06 +0300

    Add create and remove dates to project contracts

---
+++ src/Models/Contracts/Project/ProjectUserCreateDateInfo.cs
@@ -0,0 +1,44 @@
+using LT.DigitalOffice.Models.Broker.Enums;
+using System;
+
+namespace DigitalOffice.Models.Contracts.Project
+{
+  public record ProjectUserCreateDateInfo(
+    Guid Id,
+    Guid UserId,
+    Guid ProjectId,
+    ProjectUserRoleType Role,
+    DateTime CreateAtUtc,
+    Guid CreatedBy)
+  {
+    /// <summary>
+    /// Create date identifier.
+    /// </summary>
+    public Guid Id { get; init; } = Id;
+
+    /// <summary>
+    /// User identifier to create.
+    /// </summary>
+    public Guid UserId { get; init; } = UserId;
+
+    /// <summary>
+    /// Project identifier.
+    /// </summary>
+    public Guid ProjectId { get; init; } = ProjectId;
+
+    /// <summary>
+    /// Project user role.
+    /// </summary>
+    public ProjectUserRoleType Role { get; init; } = Role;
+    
+    /// <summary>
+    /// Project user's create time.
+    /// </summary>
+    public DateTime CreateAtUtc { get; init; } = CreateAtUtc;
+
+    /// <summary>
+    /// Date creator's Id.
+    /// </summary>
+    public Guid CreatedBy { get; init; } = CreatedBy;
+  }
+}
