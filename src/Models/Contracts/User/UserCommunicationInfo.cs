51d6e9d06a3794ca4b1c7777f2c297c7393137d8
Ryzhenkova Ekaterina <vadivazikryzh@gmail.com>
2025-04-16 14:12:45 +0300

    Add Id to UserCommunicationInfo

--- src/Models/Contracts/User/UserCommunicationInfo.cs
+++ src/Models/Contracts/User/UserCommunicationInfo.cs
@@ -1,12 +1,20 @@
+using System;
+
 namespace DigitalOffice.Models.Contracts.User
 {
   /// <summary>
   /// Communication information.
   /// </summary>
   public record UserCommunicationInfo(
+    Guid Id,
     CommunicationType Type,
     string Value)
   {
+    /// <summary>
+    /// Communication Id.
+    /// </summary>
+    public Guid Id { get; init; } = Id;
+    
     /// <summary>
     /// Communication type.
     /// </summary>
